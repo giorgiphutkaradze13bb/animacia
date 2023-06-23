cc16acb13f71cc8c5112c13a24b0a7ae7d82616a
giorgiphutkaradze13bb <133670991+giorgiphutkaradze13bb@users.noreply.github.com>
2023-06-23 11:53:17 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let students = 30//სტუდენტის რაოდენობა
+let st = 10//გარიცხული სტუდენტები
+let str = students * st/100//
+students=students + str //ახალი სტუდენტების რაოდენობა
+console.log(str);
+console.log(students);
